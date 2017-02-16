076c6f22c5565296c6c3b3017615457a8a627384
Andrey Demidov <andrey.demidov@gmail.com>
2017-02-16 20:52:29 +0300

    fix: fetch reddits

--- reddit/reddit.go
+++ reddit/reddit.go
@@ -6,7 +6,18 @@ import (
 
 	"log"
 	"time"
+	"net/http"
 )
+
+func fetchFunc(url string) (resp *http.Response, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println("reddit: cannot fetch url " + url)
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405")
+	return http.DefaultClient.Do(req)
+}
 
 func Fetch() {
 
@@ -16,7 +27,7 @@ func Fetch() {
 		if db_reddit.LastSyncTime + int64(db_reddit.SyncInterval) < time.Now().Unix() {
 
 			log.Println("fetch reddit: " + db_reddit.Link)
-			f, err := rss.Fetch(db_reddit.Link)
+			f, err := rss.FetchByFunc(fetchFunc, db_reddit.Link)
 
 			if err != nil {
 				log.Println(err)
