ac0ac59b16e1002491e5a4ee6c4077f9eedae3e9
Andrey Demidov <andrey.demidov@gmail.com>
2017-03-18 07:45:01 +0300

    refactore: format code

--- pkg/postgres/stackoverflow.go
+++ pkg/postgres/stackoverflow.go
@@ -1,11 +1,12 @@
 package postgres
 
 import (
-	"github.com/demas/cowl-go/pkg/quzx-crawler"
-	"github.com/demas/cowl-go/pkg/classificator"
-	"strings"
-	"log"
 	"fmt"
+	"log"
+	"strings"
+
+	"github.com/demas/cowl-go/pkg/classificator"
+	"github.com/demas/cowl-go/pkg/quzx-crawler"
 )
 
 func Insert_so_Questions(questions []quzx_crawler.SOQuestion, site string) {
@@ -21,14 +22,28 @@ func Insert_so_Questions(questions []quzx_crawler.SOQuestion, site string) {
 			log.Fatal(err)
 		}
 
+		insertQuery := `INSERT INTO stackquestions(title, link, questionid, tags, score, answercount, viewcount, userid, userreputation, userdisplayname, 
+		                                           userprofileimage, classification, details, creationdate, readed) 
+						VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`
+
 		if id == 0 {
-			_, err = tx.Exec("INSERT INTO stackquestions(" +
-				"title, link, questionid, tags, score, answercount, viewcount, userid, userreputation, " +
-				"userdisplayname, userprofileimage, classification, details, creationdate, readed) " +
-				"VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)",
-				q.Title, q.Link, q.Question_id, strings.Join(q.Tags[:], ","), q.Score, q.Answer_count, q.View_count,
-				q.Owner.User_id, q.Owner.Reputation, q.Owner.Display_name, q.Owner.Profile_image,
-				classification, details, q.Creation_date, 0)
+
+			_, err = tx.Exec(insertQuery,
+				q.Title,
+				q.Link,
+				q.Question_id,
+				strings.Join(q.Tags[:], ","),
+				q.Score,
+				q.Answer_count,
+				q.View_count,
+				q.Owner.User_id,
+				q.Owner.Reputation,
+				q.Owner.Display_name,
+				q.Owner.Profile_image,
+				classification,
+				details,
+				q.Creation_date,
+				0)
 
 			if err != nil {
 				log.Fatal(err)
