627b2e4fe940f6ee9189b8972b62fdb874223a2e
Andrey Demidov <andrey.demidov@gmail.com>
2017-07-06 14:31:36 +0300

    changes

--- main.go
+++ main.go
@@ -3,20 +3,10 @@ package main
 import (
 	_ "github.com/lib/pq"
 
-	"log"
-	"os"
-
 	"github.com/demas/cowl-go/pkg/services"
 )
 
 func main() {
-
-	args := os.Args[1:]
-
-	if len(args) == 0 {
-		log.Println("Specify command")
-		return
-	}
 
 	services.FetchNews()
 }
